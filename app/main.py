a601b8ea410ea7ffc22d45b0b9a48b1e6660d5ea
HAms25 <hilari.martinez@tecsup.edu.pe>
2025-03-20 22:24:53 -0500

    Add create note endpoint :wrench:

--- app/main.py
+++ app/main.py
@@ -1,7 +1,58 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from fastapi import HTTPException
+import uuid
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "message": "Bienvenido a la API de notas!" }
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+
+#Definir modelo de datos
+class Note(NoteCreate):
+    id:str
+
+#Base de datos
+notes_db: List[Note] = []
+
+#Endpoint para obtener todas las notas
+
+#Obtener notas
+@app.get("/notes")
+def get_notes():
+    return notes_db
+
+#Crear notas
+@app.post("/notes")
+def create_note(note_data: NoteCreate):
+    for n in notes_db:
+        if n.title == note_data.title:
+            raise HTTPException(status_code=400, detail="Ya existe una nota con este titulo")
+        
+    new_note = Note(id=str(uuid.uuid4()), **note_data.dict())
+    notes_db.append(new_note)
+    return {"message": "Nota creada", "note": new_note}
+
+#Actualizar nota
+@app.put("/notes/{note_id}")
+def update_note(note_id: int, note: Note):
+    for i, n in enumerate(notes_db):
+        if n.id == note_id:
+            notes_db[i] = note
+            return {"message": "Nota actualizada", "note": note}
+    
+    raise HTTPException(status_code=404, detail="Nota no encontrada")
+
+#Eliminar nota
+@app.delete("/notes/{note_id}")
+def delete_note(note_id: int):
+    for i, n in enumerate(notes_db):
+        if n.id == note_id:
+            del notes_db[i]
+            return {"message": "Nota eliminada"}
+        
+    raise HTTPException(status_code=404, detail="Nota no encontrada")
+        
+    
